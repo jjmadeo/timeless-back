32c4a6873449c722fdb9030be644e7c8b70ba42e
juan <jjmadeo@gmail.com>
2024-11-14 10:00:31 -0300

    cambios alta empresa parametro

--- src/main/java/upe/edu/demo/timeless/repository/ConfigSistemaRepository.java
+++ src/main/java/upe/edu/demo/timeless/repository/ConfigSistemaRepository.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Repository;
 import upe.edu.demo.timeless.model.ConfigSistema;
 import upe.edu.demo.timeless.model.TipoUsuario;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfigSistemaRepository extends CrudRepository<ConfigSistema, Integer> {
+
+
+
+
+
+    Optional<ConfigSistema> findByClave(String clave);
 }
